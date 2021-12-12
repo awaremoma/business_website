0944b84dd3e2aea44ddd334d04ffbf2fa96566e5
Alex Watkins <38054672+afw821@users.noreply.github.com>
2021-12-11 22:53:24 -0500

    added the service files and the dot env file

--- startup/db.js
+++ startup/db.js
@@ -2,7 +2,7 @@ const db = require("../models");
 
 module.exports = (app) => {
   db.sequelize.sync().then( () => {
-    const PORT = process.env.PORT || 3800;
+    const PORT = process.env.PORT || 3801;
     app.listen(PORT, () => console.log(`App listening on PORT: ${PORT}`));
   });
 };
